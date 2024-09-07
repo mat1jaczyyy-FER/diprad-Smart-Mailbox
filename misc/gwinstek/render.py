f3469330b085dc81a44067a54cde4693954db16b
mat1jaczyyy <mat1jaczyyy@gmail.com>
2024-09-07 07:24:02 +0200

    misc: doc is being worked on

--- misc/gwinstek/render.py
+++ misc/gwinstek/render.py
@@ -1,11 +1,14 @@
 import os
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
 
 to_watts=3.3
 smoothing_threshold=6E-3*to_watts
 
 def plot_waveform(csv_file):
+    name = os.path.splitext(os.path.basename(csv_file))[0]
+    color = '#d62728' if name == 'INFRARED' else '#1f77b4'
     x_values = []
     y_values = []
 
@@ -32,21 +35,53 @@ def plot_waveform(csv_file):
 
     plt.figure(figsize=(12, 4))
 
-    plt.plot(smoothed_x, smoothed_y, linewidth=0.3)
-    plt.xlabel('Time [t]')
+    plt.plot(smoothed_x, smoothed_y, linewidth=0.3, color=color)
+    plt.xlabel('Time [s]')
     plt.ylabel('Power draw [W]')
+    plt.xticks(range(0, int(smoothed_x[-1]) + 2, 1))
     plt.grid(True)
 
-    # Set transparent background
-    plt.gcf().set_facecolor('none')
-    plt.gca().xaxis.label.set_color('white')  # Set x-axis label color to white
-    plt.gca().yaxis.label.set_color('white')  # Set y-axis label color to white
-    plt.gca().tick_params(axis='both', colors='white')  # Set tick colors to white
-
-    plot_filename = os.path.splitext(os.path.basename(csv_file))[0] + '.svg'
+    plot_filename = name + '.svg'
     plt.savefig(plot_filename, format='svg')
 
     plt.close()
+
+    dx = 0.004
+    if name == 'ULTRASONIC':
+        mask = 12.5
+        mask_precise = (12.58, 14.46)
+    elif name == 'INFRARED':
+        mask = 11.65
+        mask_precise = (11.724, 12.86)
+    else:
+        return
+    
+    smoothed_x = np.array(smoothed_x)
+    smoothed_y = np.array(smoothed_y)
+
+    filtered_x = smoothed_x[(smoothed_x >= mask) & (smoothed_x <= mask + 2)]
+    filtered_y = smoothed_y[(smoothed_x >= mask) & (smoothed_x <= mask + 2)]
+
+    plt.figure(figsize=(9, 3))
+
+    plt.plot(filtered_x, filtered_y, linewidth=1, color=color)
+    plt.xlabel('Time [s]')
+    plt.ylabel('Power draw [W]')
+    plt.xticks(np.arange(0, int(smoothed_x[-1]) + 2, 0.2))
+    plt.grid(True)
+
+    plt.xlim(mask, mask + 2)
+    plt.ylim(-0.02, 0.22)
+    plt.tight_layout()
+
+    plot_filename = name + '_ZOOM.svg'
+    plt.savefig(plot_filename, format='svg')
+
+    plt.close()
+
+    y_hill = y_values[int(mask_precise[0] / dx):int(mask_precise[1] / dx)]
+    energy = (dx/2) * (y_hill[0] + y_hill[-1] + 2 * sum(y_hill[1:-1])) # trapez
+    print(f"{name}: {energy} J")
 
 current_directory = os.getcwd()
 csv_files = [file for file in os.listdir(current_directory) if file.endswith('.csv')]
--- misc/sensors/ultrasonic.py
+++ misc/sensors/ultrasonic.py
@@ -10,14 +10,14 @@ def plot_sensors(csv_file):
         data = [row for row in csv_reader]
     
     measurements = [row[0] for row in data]
-    cleaned = [float(value) for value in measurements if value != "fail"]
+    cleaned = [float(value) * 10 for value in measurements if value != "fail"]
 
-    bins = np.arange(0, 16 + 0.2, 0.2)
+    bins = np.arange(0, 160 + 2, 2)
     fig, ax = plt.subplots(figsize=(12, 4))
     counts, edges, patches = ax.hist(cleaned, bins=bins, color='skyblue', edgecolor='black')
-    ax.set_xlim(0, 16)
+    ax.set_xlim(0, 160)
 
-    highlight_range = (5.4, 5.8)
+    highlight_range = (54, 58)
     for i in range(len(edges) - 1):
         bin_start = round(edges[i], 2)
         bin_end = round(edges[i + 1], 2)
@@ -25,9 +25,9 @@ def plot_sensors(csv_file):
         if bin_start < highlight_range[1] and bin_end > highlight_range[0]:
             patches[i].set_facecolor('red')
 
-    ax.set_xlabel(header[0])
+    ax.set_xlabel(header[0] + ' [mm]')
     ax.set_ylabel('Frequency')
-    ax.set_xticks(np.arange(0, 16, 0.8))
+    ax.set_xticks(np.arange(0, 160, 8))
 
     plot_filename = os.path.splitext(os.path.basename(csv_file))[0] + '.svg'
     plt.savefig(plot_filename, format='svg')
@@ -35,7 +35,7 @@ def plot_sensors(csv_file):
     plt.close()
 
 current_directory = os.getcwd()
-csv_files = [file for file in os.listdir(current_directory) if file == 'ultrasound.csv']
+csv_files = [file for file in os.listdir(current_directory) if file == 'ultrasonic.csv']
 
 for csv_file in csv_files:
     plot_sensors(csv_file)
