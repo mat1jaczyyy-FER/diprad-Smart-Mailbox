9bc5474ee650bc79bf8b25b8d942c165429e2175
mat1jaczyyy <mat1jaczyyy@gmail.com>
2024-09-09 06:06:43 +0200

    web: /api/mailbox/notify db last_seen and last_push

--- web/src/pages/api/mailbox/notify.ts
+++ web/src/pages/api/mailbox/notify.ts
@@ -2,5 +2,11 @@ import type { APIRoute } from "astro";
 import { db } from "../../../utils/db";
 
 export const POST: APIRoute = async ({ request }) => {
-    throw new Error("Not implemented");
+    const req = await request.json();
+
+    await db.request()
+        .input('private_code', req.private_code)
+        .execute('sp_mailbox_notify');
+    
+    return new Response(null, { status: 200 });
 };
